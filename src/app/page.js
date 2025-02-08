fd018f913ff3de399a9fc08c01ad2458267c3a25
Richard Marafioti <richmarafioti@gmail.com>
2025-02-08 14:22:02 -0600

    condensed styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
 "use client";
-import Image from "next/image";
-import styles from "./page.module.css";
 import { useState } from "react";
 
-import style from "@/app/styling/landingP.module.css";
+import styles from "@/app/styling/landingP.module.css";
+
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -11,100 +10,19 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>My Personal Website</h1>
-        {isLoading && <div className={style.skeleton}>Loading Form...</div>}
+        {isLoading && <div className={styles.skeleton}>Loading Form...</div>}
         <iframe
           src="https://dashboard.mailerlite.com/forms/1319105/145608201780331689/share"
-          className={style.newsletter}
+          className={styles.newsletter}
           scrolling="no"
           style={{
             display: isLoading ? "none" : "block",
           }}
           onLoad={() => setIsLoading(false)}
         ></iframe>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <h2>footer section.</h2>
       </footer>
     </div>
   );
